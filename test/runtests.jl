8e20ec39ec0532663866c537c94603269eca1424
Tim Holy <tim.holy@gmail.com>
2016-09-11 10:39:27 -0500

    Support converting while preserving the settings

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,13 @@
 using ComputationalResources
 using Base.Test
 
+for r in (CPU1(5), CPUThreads(5), ArrayFireLibs(5), CUDALibs(5), OpenCLLibs(5))
+    for T in (CPU1, CPUThreads, ArrayFireLibs, CUDALibs, OpenCLLibs)
+        r1 = T(r)
+        @test r1.settings === 5
+    end
+end
+
 push!(LOAD_PATH, joinpath(dirname(@__FILE__), "packages"))
 
 import Dummy1
